a10f2951bc0e2ae12e32e335ed237e94d6daae56
Fabian Ardila Castro <fabian.ardila@scotiabankcolpatria.com>
2022-08-31 10:50:21 -0500

    Stateless Widget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
+import 'src/shared/extensions.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    const fontSize30 = TextStyle(fontSize: 30);
+    int counter = 10;
+
+    return Scaffold(
       appBar: AppBar(
         title: const Text("Home Screen"),
         elevation: 0,
       ),
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text("Hola Mundo"),
-          Text("Hola Mundo"),
-        ],
-      )),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Numero de click",
+              style: fontSize30,
+            ),
+            Text(
+              '$counter',
+              style: fontSize30,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.add),
+        onPressed: () {
+          print("Hola mundo");
+          counter++;
+        },
+        label: const Text("Nuevo"),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: HomeScreen()
+    return MaterialApp(
+      theme: ThemeData(colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+      home: const HomeScreen(),
     );
   }
 }
